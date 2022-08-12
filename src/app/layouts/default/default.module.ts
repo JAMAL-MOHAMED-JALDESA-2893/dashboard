e28e51bb3b18ec934fb0ea2218d08c55fd731954
jamal <jayhamo22@gmail.com>
2022-08-12 20:22:36 +0300

    testing angular material components

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,6 +1,3 @@
-import { FooterComponent } from './../../shared/components/footer/footer.component';
-import { HeaderComponent } from './../../shared/components/header/header.component';
-import { SidebarComponent } from './../../shared/components/sidebar/sidebar.component';
 import { PostsComponent } from './../../modules/posts/posts.component';
 import { RouterModule } from '@angular/router';
 import { DefaultComponent } from './default.component';
@@ -8,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 
 
@@ -21,7 +19,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule
+    SharedModule,
+    MatSidenavModule
   ]
 })
 export class DefaultModule { }
